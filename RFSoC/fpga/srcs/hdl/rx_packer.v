1a6891a703d67eebde25bc09a3e48b1377d8ee2b
John M Mower <mowerj@uw.edu>
2023-11-21 11:51:45 -0800

    updated rx-packer

--- RFSoC/fpga/srcs/hdl/rx_packer.v
+++ RFSoC/fpga/srcs/hdl/rx_packer.v
@@ -5,18 +5,21 @@ module rx_packer #(parameter DEPTH=1024, parameter SIM=0)
   (
    input 	      clk,
 
-   input 	      run,      // state reset at rising edge
-   input 	      trig_int, // rx int trig one-shot
-   input              trig,     // rx trig one-shot
+   input 	      run,       // state reset at rising edge
+   input              trig,      // rx trig       ---\
+   input 	      trig_strt, // rx start trig ------ one-shot
+   input              trig_last, // rx last trig  ---/
 
-   input [31:0]       sec,      // epoch
-   input [31:0]       tic,      // free run tic
-   input [1:0]        ant,      // current antenna
+   input [31:0]       sec,       // epoch
+   input [31:0]       tic,       // free run tic
+   input [1:0]        ant,       // current antenna set before trig is relayed
 
    // p vals are assumed static for triggering period
-   input [15:0]       pcfg,     // data tag
-   input [15:0]       psamps,   // number of samples, always psamps & 0xFFFC
-   input [15:0]       pshift,   // number of bits to trim
+   input [15:0]       pcfg,      // data tag
+   input [15:0]       psamps,    //
+   input [15:0]       psampsm1,  // number of samples, always psamps & 0xFFFC - 1
+   input [15:0]       pshift,    // number of bits to trim
+   input [31:0]       pdelaym1,  // tics to delay to trig -1
    
    input [15:0]       rx_I,
    input [15:0]       rx_Q,
@@ -26,23 +29,31 @@ module rx_packer #(parameter DEPTH=1024, parameter SIM=0)
    output reg         tvalid,
    output reg         tlast,
    
-   output reg         error
+   output reg         buf_error,
+   output reg         trg_error
    );
 
    wire rst = ~run;
 
    wire accum_vld;
-   wire accum_vld_m1;
-   
+   wire accum_lst;
    wire [15:0] accum_I;
    wire [15:0] accum_Q;
-
+   
+   reg 	       buflst;
    reg [127:0] bufdat;
    reg [3:0]   bufvld;
-   reg [3:0]   buflst;
-         
-   wire [15:0] samps = psamps & 16'hFFFC;
 
+   reg 	       running;
+
+   reg 	       trig_cght;
+   reg 	       trig_strt_cght;
+   reg 	       trig_last_cght;
+   reg [31:0]  trig_cntr;
+   reg 	       itrig;
+   reg 	       itrig_strt;
+   reg 	       itrig_last;
+   
    always @(posedge clk) begin
 
       bufdat <= {bufdat[95:0], accum_Q, accum_I};
@@ -55,49 +66,94 @@ module rx_packer #(parameter DEPTH=1024, parameter SIM=0)
       else
 	bufvld <= 0;
 
-      if (accum_vld && ~accum_vld_m1)      
-	buflst <= {buflst[2:0], 1'b1};
-      else
-	buflst <= buflst << 1;
-                
-      if (trig && trig_int)
+      buflst <= accum_lst;
+   
+   end         
+      
+   always @(posedge clk) begin
+      
+      if (itrig && itrig_strt)
 	tdata <= {`SYNC, pcfg,
                   sec,
                   tic,
-                  6'b000000, ant, 8'h0, samps};
+                  6'b000000, ant, 8'h0, psamps};
       else 
 	tdata <= bufdat;
 
-      tvalid <= (trig && trig_int) || &bufvld;
+      tvalid <= (itrig && itrig_strt) || &bufvld;
       
-      tlast <= |buflst;
+      tlast <= buflst;
 
+      end
+
+   always @(posedge clk) begin
+	    
       if (rst)
-	error <= 0;
+	running <= 0;
+      else
+	if (trig)
+	  running <= 1;
+	else if (tlast)
+	  running <= 0;
+      
+      if (trig)
+	trig_cntr <= 0;
+      else
+	trig_cntr <= trig_cntr + 'b1;
+      
+      if (trig) begin
+	 trig_cght <= 1;
+	 trig_strt_cght <= trig_strt;
+	 trig_last_cght <= trig_last;
+      end
+      else if (trig_cntr == pdelaym1) begin
+	 trig_cght <= 0;
+	 trig_strt_cght <= 0;
+	 trig_last_cght <= 0;
+      end
+
+      if (running && trig_cght && trig_cntr == pdelaym1) begin
+	 itrig <= 1;
+	 itrig_strt <= trig_strt_cght;
+	 itrig_last <= trig_last_cght;
+      end
+      else begin
+	 itrig <= 0;
+	 itrig_strt <= 0;
+	 itrig_last <= 0;
+      end	
+      
+      if (rst)
+	buf_error <= 0;
       else
 	if (tvalid && ~tready)
-	  error <= 1;
+	  buf_error <= 1;
+
+      if (rst)
+	trg_error <= 0;
+      else
+	if (trig && running)
+	  trg_error <= 1;
             
    end
-   
+      
    accumulator #(.DEPTH(DEPTH), .SIM(SIM))
    accumulator_inst
      (
-      .clk        (clk         ),
-      .rst        (rst         ),
-      .trig       (trig        ),
-      .trig_int   (trig_int    ),
-      .shift      (pshift      ),
-      .samps      (samps       ),
-      .din_I      (rx_I        ),
-      .din_Q      (rx_Q        ),
-      .dout_I     (accum_I     ),
-      .dout_Q     (accum_Q     ),
-      .dout_vld   (accum_vld   ),
-      .dout_vld_m1(accum_vld_m1),
-      .mon_I      (            ),
-      .mon_Q      (            ),
-      .mon_vld    (            )
+      .clk      (clk         ),
+      .rst      (rst         ),
+      .trig     (itrig       ),
+      .trig_strt(itrig_strt  ),
+      .trig_last(itrig_last  ),
+      .shift    (pshift      ),
+      .sampsm1  (psampsm1    ),
+      .din_I    (rx_I        ),
+      .din_Q    (rx_Q        ),
+      .dout_I   (accum_I     ),
+      .dout_Q   (accum_Q     ),
+      .dout_vld (accum_vld   ),
+      .dout_lst (accum_lst   )
       );
       
-endmodule
+endmodule 
+
--- RFSoC/fpga/srcs/tb/tb_rx_packer.sv
+++ RFSoC/fpga/srcs/tb/tb_rx_packer.sv
@@ -8,16 +8,18 @@
 `define SIG  -70.0
 `define SNR  -10.0
 
-`define DEPTH 4096
-`define SAMPS 32
-`define COUNT 16
+`define DEPTH 64
+`define SAMPS 16
+`define COUNT 1
+`define DELAY 16
 
 module tb_rx_packer;
 
    reg clk = 0;
    reg rst = 0;
    reg trig=0;
-   reg trig_int=0;
+   reg trig_strt=0;
+   reg trig_last=0;
    reg signed [15:0] dataI;
    reg signed [15:0] dataQ;
 
@@ -35,8 +37,9 @@ module tb_rx_packer;
    wire 	tvalid;
    wire 	tlast;
    reg 		tready = 1;
-   wire 	error;
-   
+   wire 	buf_error;
+   wire 	trg_error;
+      
    localparam tone = real'(`CLKF) / 64.0;
    localparam dbfs = 20.0 * $log10(2**15);
    localparam mags = 10**((dbfs+`SIG)/20.0);
@@ -44,6 +47,8 @@ module tb_rx_packer;
    localparam dlys = 2.0 * real'(`DEPTH) / real'(`CLKF) / 1e-9;
    localparam [15:0] shift = $clog2(`COUNT);
    localparam [15:0] samps = `SAMPS;
+   localparam [15:0] sampsm1 = `SAMPS - 1;
+   localparam [31:0] delaym1 = `DELAY - 1;
       
    integer     i;
 
@@ -80,22 +85,23 @@ module tb_rx_packer;
       rst = 0;
    endtask
 
-   task do_trig(input i);
+   task do_trig(input [1:0] i);
       wait(clk == 0);
       trig = 1;
-      if (i)
-	trig_int = 1;
+      if (i[0])
+	trig_strt = 1;
+      if (i[1])
+	trig_last = 1;
       wait(clk == 1);
       wait(clk == 0);
       trig = 0;
-      trig_int = 0;
+      trig_strt = 0;
+      trig_last = 0;
    endtask      
    
    initial forever #(1.0/`CLKF/2.0/1e-9) clk = ~clk;
 
    initial begin
-      trig = 0;
-      trig_int = 0;
       #100;
       do_reset;
       #100;
@@ -105,15 +111,18 @@ module tb_rx_packer;
       for (i=0; i<`COUNT; i=i+1) begin
 	 
 	 if (i==0)
-	   do_trig(1);
+	   if (`COUNT <= 1)
+	     do_trig(2'b11);
+	   else
+	     do_trig(2'b01);
+	 else if (i==(`COUNT-1))
+	   do_trig(2'b10);
 	 else
-	   do_trig(0);
+	   do_trig(2'b00);
+	 
 	 #(dlys);
 
       end
-
-      do_trig(1);
-      #(dlys);
 
       $finish;
 
@@ -123,21 +132,25 @@ module tb_rx_packer;
      (
       .clk(clk),
       .run(run),
-      .trig_int(trig_int),
+      .trig_strt(trig_strt),
+      .trig_last(trig_last),
       .trig(trig),
       .sec(32'hDEADBEEF),
       .tic(tic),
       .ant(ant),
       .pcfg(16'hBABE),
-      .psamps(samps),
+      .psampsm1(sampsm1),
       .pshift(shift),
+      .psamps(samps),
+      .pdelaym1(delaym1),
       .rx_I(dataI),
       .rx_Q(dataQ),
       .tdata(tdata),
       .tready(tready),
       .tvalid(tvalid),
       .tlast(tlast),
-      .error(error)
+      .buf_error(buf_error),
+      .trg_error(trg_error)
       );
          
 endmodule
