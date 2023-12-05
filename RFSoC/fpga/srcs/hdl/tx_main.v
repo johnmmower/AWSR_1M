3f25891acfa1ab667753f5b80fc1baeb320f4f4c
John M Mower <mowerj@uw.edu>
2023-12-05 12:49:10 -0800

    tx addressing added

---
+++ RFSoC/fpga/srcs/hdl/tx_main.v
@@ -0,0 +1,70 @@
+
+
+module tx_main #(parameter ADDRBITS = 13)
+  (
+   input 		     clk,
+   input 		     rst,      // opposite of run
+
+   input 		     trig,     // assumed one-show
+
+   output reg [ADDRBITS-1:0] addr,  
+   output reg 		     paen,     // turns on at trigger
+
+   // p vals are assumed static for run
+   input                     pusepa,
+   input [31:0] 	     pdelaym1, // delay to addr start
+   input [31:0] 	     ponm1     // delay to paen stop
+   );
+
+   reg [31:0] cntr;
+   
+   reg [31:0] delaym1;
+   reg [31:0] onm1;
+   reg 	      usepa;
+   
+   reg 	      rst_t1;
+   
+   always @(posedge clk) begin
+   
+      rst_t1 <= rst;
+
+      if (~rst && rst_t1) begin
+	 delaym1 <= pdelaym1;
+	 onm1 <= ponm1;
+      end
+
+      if (rst)
+	usepa <= 0;
+      else if (~rst && rst_t1)
+	usepa <= pusepa;
+
+      if (rst)
+	cntr <= 32'hFFFF_FFFF;
+      else
+	if (trig)
+	  cntr <= 0;
+	else if (cntr < 32'hFFFF_FFFF)
+	  cntr <= cntr + 'b1;
+      
+      if (rst)
+	paen <= 0;
+      else
+	if (trig && usepa)
+	  paen <= 1;
+	else if (cntr >= onm1)
+	  paen <= 0;
+
+      if (rst)
+	addr <= 0;
+      else if (trig || cntr <= delaym1)
+	addr <= 0;
+      else if (addr == { ADDRBITS {1'b1} })
+	addr <= { ADDRBITS {1'b1} };
+      else if (cntr > delaym1)
+	addr <= addr + 'b1;
+	   
+   end
+
+endmodule
+
+   
---
+++ RFSoC/fpga/srcs/tb/tb_tx_main.sv
@@ -0,0 +1,44 @@
+
+`timescale 1 ns / 1 ps
+
+`define CLKF 215040000
+
+module tb_tx_main;
+
+   reg clk = 0;
+   reg rst = 1;
+   reg trig = 0;
+
+   wire [12:0] addr;
+   wire        paen;
+
+   reg 	       pusepa = 1;
+   reg [31:0]  pdelaym1 = 15;
+   reg [31:0]  ponm1    = 31;
+   
+   initial forever #(1.0/`CLKF/2.0/1e-9) clk = ~clk;
+
+   initial begin
+      #(100.0/`CLKF/1e-9);
+      rst = 0;
+      #(100.0/`CLKF/1e-9);
+      trig = 1;
+      #(1.0/`CLKF/1e-9);
+      trig = 0;
+   end
+   
+   tx_main uut
+     (
+      .clk(clk),
+      .rst(rst),
+      .trig(trig),
+      .addr(addr),
+      .paen(paen),
+      .pusepa(pusepa),
+      .pdelaym1(pdelaym1),
+      .ponm1(ponm1)
+      );
+   
+endmodule
+
+   
